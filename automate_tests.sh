e4dd01a8052eed0fd367c49f83619e34e5ddbad3
Gabriel Lozano <golozanop@unal.edu.co>
2023-12-02 21:45:35 +0100

    adds timescale

--- automate_tests.sh
+++ automate_tests.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-declare -a useCases=("cpu-only" "iot" "devops")
-declare -a scales=("1" "2" "4" "8" "16" "32" "64" "128")
+declare -a useCases=("devops")
+declare -a scales=("10" "15")
 
 for useCase in "${useCases[@]}"
 do
 for scale in "${scales[@]}"
 do
     echo RUNNING FOR $useCase $scale
+    docker compose up -d
     sh generate-data.sh $scale $useCase
     sh benchmark-databases.sh $scale $useCase
+    docker compose down
 
 done
 done
--- build_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-
-cd tsbs-master
-docker build -t tsbs:0.1 .
-cd ..
-
-cd influx-docker
-docker build -t influxdb:2.7 .
